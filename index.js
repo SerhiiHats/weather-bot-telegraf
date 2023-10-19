a36c62c9c1d4419342b64a667c1cb3c8fa85634a
Serhi Hats <gatsserv@gmail.com>
2023-10-19 23:03:38 +0300

    rename forward file

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ app.use("/", (req, res) => {
   res.send("Server is running! Start at: " + time);
 });
 
-const bot = new Telegraf(process.env.BOT_TOKEN);                   //create object bot
+const index = new Telegraf(process.env.BOT_TOKEN);                   //create object index
 
 //обработчик команды старт
-bot.start((ctx) => {
+index.start((ctx) => {
   ctx.reply(`App ☔ the Weather:\nHello ${ctx.message.from.first_name} ❤, I can show you the weather where you are 📍 💯`, {
     ...mainMenu
   });
 });
 
 //прослушиватель на возврат в меню
-bot.hears(CMD_TEXT.menu, (ctx) => {
+index.hears(CMD_TEXT.menu, (ctx) => {
   ctx.reply('Ви у головному меню', {...mainMenu})
 });
 
 //прослушиватель на погоду по месту где находишься
-bot.hears(CMD_TEXT.weatherI, async (ctx) => {
+index.hears(CMD_TEXT.weatherI, async (ctx) => {
   const weatherOfString = await currentWeatherOfString();
   if (weatherOfString) {
     ctx.reply(weatherOfString, {...backButtonMenu});
@@ -37,12 +37,12 @@ bot.hears(CMD_TEXT.weatherI, async (ctx) => {
   }
 })
 
-bot.hears(CMD_TEXT.weatherEveryWhere, (ctx) => {
+index.hears(CMD_TEXT.weatherEveryWhere, (ctx) => {
   ctx.reply('Нажаль цей функціонал у меню поки не реалізовано ☺, але скоро буде...)', {...backButtonMenu});
 })
 
 //прослушиватель на получение от пользователя геолокации
-bot.on('message', async (ctx) => {
+index.on('message', async (ctx) => {
   if (ctx.message.location) {
     const {latitude, longitude} = ctx.message.location;
     const weatherOfString = await getCurrentWeather(latitude, longitude);
@@ -54,14 +54,14 @@ bot.on('message', async (ctx) => {
 });
 
 //запуск бота
-bot.launch();
+index.launch();
 
 
 app.listen(5000, () => console.log("Server started on PORT 5000"));
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => index.stop('SIGINT'))
+process.once('SIGTERM', () => index.stop('SIGTERM'))
 
 time = new Date().toLocaleString();
 
